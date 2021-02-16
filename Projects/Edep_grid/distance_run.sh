5ab1dff9ad00a97bb23c4ec2bc53bbfa977f0731
zhanggehui <395510548@qq.com>
2021-02-16 14:04:22 +0800

    update

---
+++ Projects/Edep_grid/distance_run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+macfile="./run.mac"
+mkdir ./data
+for((i=1;i<11;i++))
+do
+    length=$i
+    filename=dis_${length}nm.dat
+
+    str1=distance
+    str2="/IonTrack/gun/distance $length nm"
+    sed -i "/$str1/c$str2" $macfile
+    
+    str1=dumpQuantityToFile
+    str2="/score/dumpQuantityToFile boxMesh eDep $filename"
+    sed -i "/$str1/c$str2" $macfile
+    
+    ./main $macfile && mv $filename ./data
+done
