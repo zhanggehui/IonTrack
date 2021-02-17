50a8425cd7c9d5f3cedcb9f7d774e3f9ffebd8ad
zhanggehui <395510548@qq.com>
2021-02-17 13:34:02 +0800

    update

--- Projects/Edep_grid/distance_run.sh
+++ Projects/Edep_grid/distance_run.sh
@@ -10,7 +10,7 @@ for((i=0;i<11;i++)); do
     #str2="/IonTrack/gun/distance $length nm"
     #sed -i "/$str1/c$str2" $macfile
     str1='/gun/position'
-    str2="/gun/position $length nm"
+    str2="/gun/position $length 0 0 nm"
     sed -i "/$str1/c$str2" $macfile
     
     str1=dumpQuantityToFile
